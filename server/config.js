279848bf2498c61414e829e60bda229bb6fc4f94
jose armando gonzalez lopez <iam@armando.mx>
2014-03-05 01:58:19 -0600

    se agregaron un landing mas chido

--- server/config.js
+++ server/config.js
@@ -1,18 +1,20 @@
-//Facebook
-Accounts.loginServiceConfiguration.remove({
-    service:"facebook"
-});
-Accounts.loginServiceConfiguration.insert({
-    service:"facebook",
-    appId:'355198821250387',
-    secret:'83f0e90401f12ccfc74fcd853f961d5d'
-});
+ Meteor.startup(function() {
+     //Facebook
+     Accounts.loginServiceConfiguration.remove({
+         service: "facebook"
+     });
+     Accounts.loginServiceConfiguration.insert({
+         service: "facebook",
+         appId: '355198821250387',
+         secret: '83f0e90401f12ccfc74fcd853f961d5d'
+     });
 
-Accounts.loginServiceConfiguration.remove({
-    service:"google"
-});
-Accounts.loginServiceConfiguration.insert({
-    service:"google",
-    clientId:'410190650489.apps.googleusercontent.com',
-    secret:'tcu0kIf56ctv-KQYzXlUozbT'
-});
+     Accounts.loginServiceConfiguration.remove({
+         service: "google"
+     });
+     Accounts.loginServiceConfiguration.insert({
+         service: "google",
+         clientId: '410190650489.apps.googleusercontent.com',
+         secret: 'tcu0kIf56ctv-KQYzXlUozbT'
+     });
+ });
